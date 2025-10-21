de794966c8676a11b2146f612a3c2af00fd26ade
Pilar Madariaga <13519104+pilarcode@users.noreply.github.com>
2025-10-21 07:41:10 +0200

    Update server.py

--- logging/server.py
+++ logging/server.py
@@ -6,11 +6,13 @@ mcp = FastMCP(name="Demo Server")
 
 @mcp.tool()
 async def add(a: int, b: int, ctx: Context) -> int:
+    #Tool functions automatically receive 'Context' as their last argument. This object has methods for logging and reporting progress to the client.
     await ctx.info("Preparing to add...")
     await ctx.report_progress(20, 100)
 
     await asyncio.sleep(2)
-
+    # Throughout your tool function, call the info(), warning(), debug(), or error() methods to log different types of messages for the client. 
+    # Also call the report_progress() method to estimate the amount of remaining work for the tool call.
     await ctx.info("OK, adding...")
     await ctx.report_progress(80, 100)
 
